70347cc0ed0cd75802f3558d1c237d4d0790c13b
FatChocobo <maplerecall@live.com>
2024-10-30 22:54:26 +0800

    CN 7.0

--- Distance/BNpcAggroInfo.cs
+++ Distance/BNpcAggroInfo.cs
@@ -50,34 +50,34 @@ internal static class BNpcAggroInfo
 			//	Verify entries against the english name in the sheet as a sanity check.  Remove those that no longer match, or have invalid TerritoryType.
 			ExcelSheet<Lumina.Excel.GeneratedSheets.TerritoryType> territorySheet = dataManager.GetExcelSheet<Lumina.Excel.GeneratedSheets.TerritoryType>();
 			ExcelSheet<Lumina.Excel.GeneratedSheets.BNpcName> BNpcNameSheet = dataManager.GetExcelSheet<Lumina.Excel.GeneratedSheets.BNpcName>( Dalamud.Game.ClientLanguage.English );
-			for( int i = mKnownAggroEntities.Count - 1; i >= 0; i-- )
-			{
-				if( mKnownAggroEntities[i].TerritoryType < 1 || territorySheet.GetRow( mKnownAggroEntities[i].TerritoryType ) == null )
-				{
-					Service.PluginLog.Debug( $"Aggro data entry removed because no such TerritoryType ID exists: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName}" );
-					mKnownAggroEntities.RemoveAt( i );
-				}
-				else if( mKnownAggroEntities[i].BNpcID < 1 || BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ) == null )
-				{
-					Service.PluginLog.Debug( $"Aggro data entry removed because no such BNpcName ID exists: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName}" );
-					mKnownAggroEntities.RemoveAt( i );
-				}
-				else if( !mKnownAggroEntities[i].EnglishName.Equals( BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular, StringComparison.InvariantCultureIgnoreCase ) )
-				{
-					//	Ignore BNpc name mismatch if it's an RSV value.  There's probably not a reliable way to handle RSV'd names without shipping
-					//	manual mappings, and doing that has some issues unless they were included with Dalamud itself.  It's already extremly unlikely
-					//	that a name ID ever gets reassigned anyway, and since RSVs are only for new content so far, any issues should be quickly noticed.
-					if( BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular.ToString().Contains( $"_rsv_{mKnownAggroEntities[i].BNpcID}" ) )
-					{
-						Service.PluginLog.Debug( $"Aggro data entry BNpcName mismatch ignored due to RSV: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName} (The game says \"{BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular}\" is the name for this ID.)" );
-					}
-					else
-					{
-						Service.PluginLog.Debug( $"Aggro data entry removed because BNpcName mismatch: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName} (The game says \"{BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular}\" is the name for this ID.)" );
-						mKnownAggroEntities.RemoveAt( i );
-					}
-				}
-			}
+			//for( int i = mKnownAggroEntities.Count - 1; i >= 0; i-- )
+			//{
+			//	if( mKnownAggroEntities[i].TerritoryType < 1 || territorySheet.GetRow( mKnownAggroEntities[i].TerritoryType ) == null )
+			//	{
+			//		Service.PluginLog.Debug( $"Aggro data entry removed because no such TerritoryType ID exists: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName}" );
+			//		mKnownAggroEntities.RemoveAt( i );
+			//	}
+			//	else if( mKnownAggroEntities[i].BNpcID < 1 || BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ) == null )
+			//	{
+			//		Service.PluginLog.Debug( $"Aggro data entry removed because no such BNpcName ID exists: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName}" );
+			//		mKnownAggroEntities.RemoveAt( i );
+			//	}
+			//	else if( !mKnownAggroEntities[i].EnglishName.Equals( BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular, StringComparison.InvariantCultureIgnoreCase ) )
+			//	{
+			//		//	Ignore BNpc name mismatch if it's an RSV value.  There's probably not a reliable way to handle RSV'd names without shipping
+			//		//	manual mappings, and doing that has some issues unless they were included with Dalamud itself.  It's already extremly unlikely
+			//		//	that a name ID ever gets reassigned anyway, and since RSVs are only for new content so far, any issues should be quickly noticed.
+			//		if( BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular.ToString().Contains( $"_rsv_{mKnownAggroEntities[i].BNpcID}" ) )
+			//		{
+			//			Service.PluginLog.Debug( $"Aggro data entry BNpcName mismatch ignored due to RSV: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName} (The game says \"{BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular}\" is the name for this ID.)" );
+			//		}
+			//		else
+			//		{
+			//			Service.PluginLog.Debug( $"Aggro data entry removed because BNpcName mismatch: {mKnownAggroEntities[i].TerritoryType}, {mKnownAggroEntities[i].BNpcID}, {mKnownAggroEntities[i].EnglishName} (The game says \"{BNpcNameSheet.GetRow( mKnownAggroEntities[i].BNpcID ).Singular}\" is the name for this ID.)" );
+			//			mKnownAggroEntities.RemoveAt( i );
+			//		}
+			//	}
+			//}
 		}
 
 		FilterAggroEntities( mCurrentFilteredTerritoryType, true );
